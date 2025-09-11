2ab9cee0e2df9ea60cec25875bf8a7fa71f4c91f
richard-syKim <richard13431343@gmail.com>
2025-09-10 23:36:59 -0400

    Add SparseArrays, GraphBLAS, and custom implmentation

--- src/spmm-anl.jl
+++ src/spmm-anl.jl
@@ -2,21 +2,24 @@ using Pkg
 Pkg.add("StatsBase")
 Pkg.add("BenchmarkTools")
 Pkg.add("JSON")
+Pkg.add("SuiteSparseGraphBLAS")
+Pkg.add("Finch")
 
 using StatsBase
 using BenchmarkTools
 using JSON
 using SparseArrays
+using SuiteSparseGraphBLAS
+using Finch
 
 const SIZE = 4096
-
 
 # s / SIZE is the density of the matrix
 function sparseMatSetup(s, M = SIZE)
     # initialize u with random int
     u = zeros(M, M)
 
-    # fill in M - s entries
+    # fill in s entries
     all_coords = collect(Iterators.product(1:M, 1:M))
     coords = sample(all_coords, s, replace=false)
     
@@ -29,25 +32,103 @@ function sparseMatSetup(s, M = SIZE)
     return u
 end
 
+function customSparseMatSetup(s, M = SIZE)
+    u = []
+
+    # fill in s entries
+    all_coords = collect(Iterators.product(1:M, 1:M))
+    coords = sample(all_coords, s, replace=false)
+
+    for coord in coords
+        i, j = coord
+        
+        push!(arr, (i, j, rand(Float64)))
+    end
+
+    return u
+end
+
+function customSparseMatMul(u, x, M = SIZE)
+    y = zeros(M, M)
+
+    for (i, j, v) in u
+        for d in 1:M
+            y[i, d] += v * x[j, d]
+        end
+    end
+
+    return y
+end
+
 
 global i = 0
-global pairs=  []
+global pairs_naive = []
+
 while i <= SIZE
     m_naive = sparseMatSetup(i)
     x = randn(SIZE, SIZE)
+
     local bench_results = @benchmark $m_naive * $x
-
-    push!(pairs, (i / SIZE, minimum(bench_results.times)))
-    println("Naive: \tDensity: ", i / SIZE, " Time: ", minimum(bench_results.times))
-
-
-    m_csc = sprand(SIZE, SIZE, i / SIZE)
-    bench_results = @benchmark $m_csc * $x
-    println("CSC: \tDensity: ", i / SIZE, " Time: ", minimum(bench_results.times))
+    push!(pairs_naive, (i / SIZE, minimum(bench_results.times)))
+    println("Density: ", i / SIZE, "\tNaive: ", minimum(bench_results.times), "\tns")
 
     global i += 16
 end
 
-open("spmm-anl_results-pls.json", "w") do f
-    write(f, JSON.json(pairs))
+open("../results/results-naive.json", "w") do f
+    write(f, JSON.json(pairs_naive))
+end
+
+i = 0
+global pairs_csc = []
+while i <= SIZE
+    m_csc = sprand(SIZE, SIZE, i / SIZE)
+    x = randn(SIZE, SIZE)
+
+    local bench_results = @benchmark $m_csc * $x
+    push!(pairs_csc, (i / SIZE, minimum(bench_results.times)))
+    println("Density: ", i / SIZE, "\tCSC: ", minimum(bench_results.times), "\tns")
+
+    global i += 16
+end
+
+open("../results/results-csc.json", "w") do f
+    write(f, JSON.json(pairs_csc))
+end
+
+i = 0
+global pairs_ssgblas = []
+while i <= SIZE
+    m_csc = sprand(SIZE, SIZE, i / SIZE)
+    x = randn(SIZE, SIZE)
+
+    m_SSGBLAS = GBMatrix(m_csc)
+    x_SSGBLAS = GBMatrix(x)
+
+    local bench_results = @benchmark $m_SSGBLAS * $x_SSGBLAS
+    push!(pairs_ssgblas, (i / SIZE, minimum(bench_results.times)))
+    println("Density: ", i / SIZE, "\tSSGBLAS: ", minimum(bench_results.times), "\tns")
+
+    global i += 16
+end
+
+open("../results/results-ssgblas.json", "w") do f
+    write(f, JSON.json(pairs_ssgblas))
+end
+
+i = 0
+global pairs_custom = []
+while i <= SIZE
+    m_custom = customSparseMatSetup(i)
+    x = randn(SIZE, SIZE)
+
+    local bench_results = @benchmark customSparseMatMul($m_custom, $x)
+    push!(pairs_custom, (i / SIZE, minimum(bench_results.times)))
+    println("Density: ", i / SIZE, "\tcustom: ", minimum(bench_results.times), "\tns")
+
+    global i += 16
+end
+
+open("../results/results-custom.json", "w") do f
+    write(f, JSON.json(pairs_custom))
 end
