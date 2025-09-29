c2ce2c0489f8e769f019c8179794903460845255
richard-syKim <richard13431343@gmail.com>
2025-09-29 19:56:18 -0400

    Change mat storage type using fwrite

--- src/create-mat.jl
+++ src/create-mat.jl
@@ -1,39 +1,36 @@
 using Pkg
 Pkg.add("StatsBase")
 Pkg.add("JSON3")
+Pkg.add("Finch")
+Pkg.add("HDF5")
 
 using StatsBase
 using JSON3
+using Finch
+using HDF5
 
-const SIZE = 4096 * 2
+const SIZE = 8192
 
-# s is the density of the matrix
-function create_sparse_mat(s, M = SIZE)
+function main()
+    X = Tensor(Dense(SparseList(Element(0.0))), randn(SIZE, 100))
 
-    return sprand(Float64, M, M, s)
-end
+    fwrite("ben/x.bsp.h5", X)
 
-inx = 0
-inc = 1
-cnt = 0
-X = randn(SIZE, SIZE)
+    N = 250
+    eps = 1e-6
 
-while inx <= (SIZE / 2)
-    M_dense = create_sparse_mat(inx)
+    indices = exp10.(range(log10(eps), 0, length=N))
 
-    sol = M_dense * X
+    for i in indices
+        println("Wrote ben/m",  i, ".json and ben/sol", i, ".json")
+        
+        M_dense = fsprand(Float64, SIZE, SIZE, i)
 
-    JSON3.write(string("ben/m",  inx, ".json"), collect(eachrow(M_dense)))
+        sol = Tensor(Dense(SparseList(Element(0.0))), M_dense * X)
 
-    JSON3.write(string("ben/sol", inx, ".json"), collect(eachrow(sol)))
-
-    if cnt >= 16
-        global cnt = 0
-        global inc *= 2
+        fwrite(string("ben/m",  i, ".bsp.h5"), M_dense)
+        fwrite(string("ben/sol", i, ".bsp.h5"), sol)
     end
-
-    global inx += inc
-    global cnt += 1
 end
 
-JSON3.write("ben/x.json", collect(eachrow(X)))
+main()
